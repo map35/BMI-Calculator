6452e1cb8160ac2a50204a30e0f861aa91fceb88
Mohammad Agung <diditkazama@gmail.com>
2020-01-13 11:50:54 +0700

    konek ui ke viewcontroller, tambah ibaction untuk uislider dan tombol calculate

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelHeight: UILabel!
+    @IBOutlet weak var labelWeight: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func sliderHeightChanged(_ sender: UISlider) {
+        let hieght = String(format: "%.2f", sender.value)+" m"
+        
+        labelHeight.text! = hieght
+    }
+    
+    @IBAction func sliderWeightChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)+" Kg"
+        
+        labelWeight.text! = weight
+        
+//        labelWeight.text! = "\(Int((sender.value))) Kg"
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmiResult = weight / pow(height, 2)
+        
+        print("\(height)")
+        print("\(weight)")
+        print("\(bmiResult)")
+    }
+    
 }
 
